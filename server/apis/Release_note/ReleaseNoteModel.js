18e168c7d085f1158bb5e6d6729a9902cd950b28
Abhishek gupta <abhi808gupta@gmail.com>
2022-02-05 15:51:06 +0530

    Realease Note Api Updated

--- server/apis/Release_note/ReleaseNoteModel.js
+++ server/apis/Release_note/ReleaseNoteModel.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const ReleaseNoteSchema =  new mongoose.Schema({
     ReleaseNoteName:{type:String},
-    ReleaseNoteContent:{type:String}
+    ReleaseNoteContent:{type:String},
+    date:{type:Date,default:Date.now()}
 });
 
 const ReleaseNoteModel = mongoose.model("ReleaseNoteModel",ReleaseNoteSchema);
--- server/apis/Use_case_document/useCaseModel.js
+++ server/apis/Use_case_document/useCaseModel.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const useCaseDocumentSchema =  new mongoose.Schema({
     useCaseDocumentName:{type:String},
-    useCaseDocumentTextContent:{type:String}
+    useCaseDocumentTextContent:{type:String},
+    date:{type:Date,default:Date.now()}
 });
 
 const UseCaseDocumentModel = mongoose.model("UseCaseDocumentModel",useCaseDocumentSchema);
